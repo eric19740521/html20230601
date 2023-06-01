3537df353fc18b022e76b0a80abd74fdd394df7f
New <64774638+eric19740521@users.noreply.github.com>
2023-06-01 08:01:53 +0800

    1
    1

---
+++ master.css.js
@@ -0,0 +1,17 @@
+
+import { init, Style } from '@master/css';
+
+		
+		
+Style.extend('colors', {
+    'tiffany-blue': '0abab5',
+    bee: 'f2b413',
+    purple: {
+        '': '491d8b',
+        50: 'a56eff',
+        60: '8a3ffc',
+        70: '6929c4',
+    }
+})		
+		
+init();
